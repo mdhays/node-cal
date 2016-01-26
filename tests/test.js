8ee7dc979367191b611903d54f55e5260701544a
Matt Hays <mdhays1995@gmail.com>
2016-01-26 15:50:59 -0600

    ran more tests using zellars.modifiedYear.

--- tests/test.js
+++ tests/test.js
@@ -16,19 +16,54 @@ describe('cal', () => {
 describe("zeller's congruence", () => {
   var zellars = {};
     zellars.modifiedMonth = function(year, month) {
-        if(month < 2) {
+        if(month <= 2) {
           return month + 12;
+        } else {
+          return month;
         }
     };
 
-    it('returns 13 for January', () => {
+    zellars.modifiedYear = function (year, month) {
+      if(month <= 2) {
+        return year - 1;
+      } else {
+        return year;
+      }
+    };
+
+  it('returns 13 for January', () => {
       const mod = zellars.modifiedMonth(2012, 1);
 
-
       expect(mod).to.equal(13)
-    });
-      // 2012, 2 === 14
-      // 2012, 3 === 3
+  });
+
+  it('returns 14 for february', () => {
+    const mod = zellars.modifiedMonth(2012, 2);
+
+    expect(mod).to.equal(14);
+  });
+
+  it('returns 3 for march', () => {
+    const mod = zellars.modifiedMonth(2012, 3);
+
+    expect(mod).to.equal(3);
+
+    // 2012, 2 === 14 √
+    // 2012, 3 === 3 √
+
+  });
+
+  it('returns 1999 for january, 2000', () => {
+    const mod = zellars.modifiedYear(2000, 1);
+
+    expect(mod).to.equal(1999);
+  });
+
+  it('returns 2011 for february, 2012', () => {
+    const mod = zellars.modifiedYear(2012, 2);
+
+    expect(mod).to.equal(2011);
+  });
 });
     // .modifiedYear
     // 2000, 1 === 1999
