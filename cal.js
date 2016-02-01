ea958271f675cd53651ed1a8319b6c52bd0fd688
Matt Hays <mdhays1995@gmail.com>
2016-02-01 15:02:55 -0600

    Fixed the spacing.

--- cal.js
+++ cal.js
@@ -1,29 +1,6 @@
-#!/usr/bin/env node --harmony_destructuring
-'use strict';
+#!/usr/bin/env node
 
-const zellers = require('./lib/zellers.js');
-const month = require('./lib/month.js');
+'use strict'
 
-
-
-// Takes the days in the array and returns a string.
-let days = month.months.one[1].toString();
-console.log('this is the days of the week', days);
-// Assign the returned string from days and removes the commas globally.
-let calDays = days.replace(/,/g, " ");
-
-// Gets the days of the week array and returns a string.
-let weekDays = month.weekdays.toString();
-
-// Removes the commas from the days of the week.
-let calWeekDays = weekDays.replace(/,/g, " ");
-
-
-
-
-// Returns the month (in this case, january).
-console.log(month.months.one[0]);
-
-console.log(calWeekDays);
-
-console.log(month.monthDays);
+require("babel-register");
+require("./lib/cli");
